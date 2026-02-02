b27db960c0dd26b8020dd619c695032b694f7b56
Prakash jaiswar <prakashjaiswar900@gmail.com>
2026-02-02 22:14:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Nav from './components/Nav'
+import Nav from './components/Nav.jsx'
 import w from './assets/img/w.jpg'
 
 
